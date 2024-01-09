3bc58d3d72cce0adee125d1584b97912b76f265e
Marcel Sander <msander@users.noreply.github.com>
2024-01-09 11:09:00 +0100

    add devpod

---
+++ fragments/labels/devpod.sh
@@ -0,0 +1,7 @@
+autopkgr)
+    name="Devpod"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit loft-sh devpod)
+    appNewVersion=$(versionFromGit loft-sh devpod)
+    expectedTeamID="KRRZK5TGX6"
+    ;;
