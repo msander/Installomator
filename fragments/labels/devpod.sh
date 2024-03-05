c7fdb20de01103799667816cc825b7b77ca8f24f
Marcel Sander <msander@users.noreply.github.com>
2024-03-05 17:14:14 +0100

    Update devpod.sh

--- fragments/labels/devpod.sh
+++ fragments/labels/devpod.sh
@@ -1,6 +1,11 @@
 devpod)
     name="devpod"
     type="dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        archiveName="aarch64\\.dmg"
+    elif [[ $(arch) == "i386" ]]; then
+        archiveName="x64\\.dmg"
+    fi
     downloadURL=$(downloadURLFromGit loft-sh devpod)
     appNewVersion=$(versionFromGit loft-sh devpod)
     expectedTeamID="KRRZK5TGX6"
