880f0869e43344393376e3bcb16f7b5ca1ed1b60
Frank Tripp <fregge@users.noreply.github.com>
2025-10-14 12:56:00 +0200

    Update zulip.sh
    Fixed downloadURL paths

--- fragments/labels/zulip.sh
+++ fragments/labels/zulip.sh
@@ -2,9 +2,9 @@ zulip)
     name="Zulip"
     type="dmg"
     if [[ $(arch) == i386 ]]; then
-        downloadURL="https://zulip.com/apps/download/mac"
+        downloadURL="https://zulip.com/apps/download/mac-intel"
     elif [[ $(arch) == arm64 ]]; then
-        downloadURL="https://zulip.com/apps/download/mac-arm64"
+        downloadURL="https://zulip.com/apps/download/mac"
     fi
     appNewVersion=$(curl -fsIL "$downloadURL" | grep -i ^location | sed 's/.*\/v\(.*\)\/Zulip-.*/\1/')
     expectedTeamID="66KHCWMEYB"
